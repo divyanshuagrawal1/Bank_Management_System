310d4443a85e0e368c84af5ab798d3d08366fd1a
Divyanshu Agrawal <divyanshu.24scse1410118@galgotiasuniversity.ac.in>
2025-05-23 16:05:12 +0530

    PinChange.java

--- bank/management/system/PinChange.java
+++ bank/management/system/PinChange.java
@@ -4,119 +4,124 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
-import java.util.Date;
 
 public class PinChange extends JFrame implements ActionListener {
-         JPasswordField pin , repin;
-         JButton change , back;
-         String pinnumber;
-         
-         PinChange(String pinnumber) {
-            this.pinnumber = pinnumber ;
-            setLayout(null);
+    JPasswordField pin, repin;
+    JButton change, back;
+    String pinnumber;
 
-            ImageIcon i1= new ImageIcon (ClassLoader.getSystemResource("icons/atm.jpg"));
-            Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
-            ImageIcon i3 = new ImageIcon (i2);
-            JLabel image = new JLabel(i3);
-            image.setBounds(0,0,900,900);
-            add(image);  
+    PinChange(String pinnumber) {
+        this.pinnumber = pinnumber;
+        setLayout(null);
 
-            JLabel text = new JLabel ("CHANGE YOUR PIN");
-            text.setForeground(Color.WHITE);
-            text.setFont(new Font ("System",Font.BOLD,16));
-            text.setBounds(250, 280, 500, 35);
-            image.add(text);
+        // Background image setup
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
 
-            JLabel pintext = new JLabel ("NEW PIN");
-            pintext.setForeground(Color.WHITE);
-            pintext.setFont(new Font ("System",Font.BOLD,16));
-            pintext.setBounds(165,320,180,25);
-            image.add(pintext);
+        // Title label
+        JLabel text = new JLabel("CHANGE YOUR PIN");
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        text.setBounds(250, 280, 500, 35);
+        image.add(text);
 
-            pin = new JPasswordField ();
-            pin.setFont (new Font ("Raleway",Font.BOLD,25));
-            pin.setBounds(330,320,180,25);
-            image.add(pin);
+        // New PIN label and field
+        JLabel pintext = new JLabel("NEW PIN");
+        pintext.setForeground(Color.WHITE);
+        pintext.setFont(new Font("System", Font.BOLD, 16));
+        pintext.setBounds(165, 320, 180, 25);
+        image.add(pintext);
 
-            JLabel repintext = new JLabel ("Re-Enter New PIN");
-            repintext.setForeground(Color.WHITE);
-            repintext.setFont(new Font ("System",Font.BOLD,16));
-            repintext.setBounds(165,360,180,25);
-            image.add(repintext);
-   
-            repin = new JPasswordField ();
-            repin.setFont (new Font ("Raleway",Font.BOLD,25));
-            repin.setBounds(330,360,180,25);
-            image.add(repin);
+        pin = new JPasswordField();
+        pin.setFont(new Font("Raleway", Font.BOLD, 25));
+        pin.setBounds(330, 320, 180, 25);
+        image.add(pin);
 
-            change = new JButton("CHANGE");
-            change.setBounds(355,405,105,30);
-            change.addActionListener(this);
-            image.add(change);
-   
-            back = new JButton("BACK");
-            back.setBounds(355,520,150,30);
-            back.addActionListener(this);
-            image.add(back);
+        // Re-enter PIN label and field
+        JLabel repintext = new JLabel("Re-Enter New PIN");
+        repintext.setForeground(Color.WHITE);
+        repintext.setFont(new Font("System", Font.BOLD, 16));
+        repintext.setBounds(165, 360, 180, 25);
+        image.add(repintext);
 
+        repin = new JPasswordField();
+        repin.setFont(new Font("Raleway", Font.BOLD, 25));
+        repin.setBounds(330, 360, 180, 25);
+        image.add(repin);
 
+        // Change button
+        change = new JButton("CHANGE");
+        change.setBounds(355, 405, 105, 30);
+        change.addActionListener(this);
+        image.add(change);
 
+        // Back button
+        back = new JButton("BACK");
+        back.setBounds(355, 520, 150, 30);
+        back.addActionListener(this);
+        image.add(back);
 
+        // Frame settings
+        setSize(900, 900);
+        setLocation(300, 0);
+        setUndecorated(true);
+        setVisible(true);
+    }
 
-            setSize(900,900);
-            setLocation (300,0);
-            setUndecorated(true);
-            setVisible(true);
-        }
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == change) {
+            try {
+                String npin = pin.getText();
+                String rpin = repin.getText();
 
-        public void actionPerformed(ActionEvent ae) {
-            if (ae.getSource()== change) {
-                try{
-                   String npin = pin.getText();
-                   String rpin = repin.getText();
+                // Check if PINs match
+                if (!npin.equals(rpin)) {
+                    JOptionPane.showMessageDialog(null, "Entered PIN does not match");
+                    return;
+                }
 
-                   if (!npin.equals(rpin)) {
-                         JOptionPane.showMessageDialog(null, "entered pin does not match");
-                         return;
-                   }
-                
-                   if (npin.equals("")) {
-                         JOptionPane.showMessageDialog(null, "Please enter new pin");
-                         return;
-                   }	
-                   if (rpin.equals("")) {
-                         JOptionPane.showMessageDialog(null, "Please re-enter new pin");
-                         return; 
-                   }
-                   Conn conn =new Conn(); 
-                   String query1 = "Update bank set pin ='"+rpin+"' where pin='"+pinnumber+"'";
+                // Check for empty PIN fields
+                if (npin.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please enter new PIN");
+                    return;
+                }
+                if (rpin.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Please re-enter new PIN");
+                    return;
+                }
 
-                   String query2 = "Update login set pin ='"+rpin+"' where pin='"+pinnumber+"'";
+                Conn conn = new Conn();
 
-                   String query3 = "Update signupthree set pin ='"+rpin+"' where pin='"+pinnumber+"'";
-          
-                   conn.s.executeUpdate(query1);
-                   conn.s.executeUpdate(query2);
-                   conn.s.executeUpdate(query3);
-                   
-                   JOptionPane.showMessageDialog(null, "Pin changed successfully");
-                   
-                   setVisible (false);
-                   new Transactions (rpin).setVisible(true);
+                // Update PIN in all relevant tables
+                String query1 = "UPDATE bank SET pin = '" + rpin + "' WHERE pin = '" + pinnumber + "'";
+                String query2 = "UPDATE login SET pin = '" + rpin + "' WHERE pin = '" + pinnumber + "'";
+                String query3 = "UPDATE signupthree SET pin = '" + rpin + "' WHERE pin = '" + pinnumber + "'";
 
-                      
-            } catch (Exception e) {
-                 System.out.println(e);
-            }
-            } else {
+                conn.s.executeUpdate(query1);
+                conn.s.executeUpdate(query2);
+                conn.s.executeUpdate(query3);
+
+                JOptionPane.showMessageDialog(null, "PIN changed successfully");
+
                 setVisible(false);
-                new Transactions(pinnumber).setVisible(true);
+                new Transactions(rpin).setVisible(true);
+
+            } catch (Exception e) {
+                System.out.println(e);
             }
+        } else {
+            // Back button action
+            setVisible(false);
+            new Transactions(pinnumber).setVisible(true);
         }
-        
-        public static void main (String args[]) {
-              new PinChange ("").setVisible(true);
-        } 
+    }
+
+    public static void main(String args[]) {
+        new PinChange("").setVisible(true);
+    }
 }
 
