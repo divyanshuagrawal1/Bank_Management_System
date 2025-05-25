56de65d82ba9c55ab2f3a3f5a01894a98c42fcc1
Divyanshu Agrawal <divyanshu.24scse1410118@galgotiasuniversity.ac.in>
2025-05-25 13:51:29 +0530

    Conn.java

--- bank/management/system/Conn.java
+++ bank/management/system/Conn.java
@@ -11,7 +11,7 @@ public class Conn{
     public Conn() {
         
         try {
-            
+            Class.forName("com.mysql.cj.jdbc.Driver");
             c = DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","Divyan@2006");
             s = c.createStatement();
                     
